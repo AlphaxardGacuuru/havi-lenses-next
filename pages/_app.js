ddf1daf5fa5dbf598ddf32f72fdb39ceb5246bc1
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-10-20 21:51:13 +0300

    Added PWA

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,20 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
+
+	/*
+	*
+	* Register service worker */
+	if (typeof window !== "undefined" && window?.location.href.match(/https/)) {
+		if ('serviceWorker' in navigator) {
+			window.addEventListener('load', () => {
+				navigator.serviceWorker.register('/sw.js')
+				// .then((reg) => console.log('Service worker registered', reg))
+				// .catch((err) => console.log('Service worker not registered', err));
+			})
+		}
+	}
+
 	return (
 		<>
 			<Header />
